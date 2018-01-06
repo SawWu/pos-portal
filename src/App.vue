d875c13e02889526395c71408ecb0286baf6926b
SawWu <2804265880@qq.com>
2018-01-06 22:08:55 +0800

    完成左边导航

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <Nav/>
     <router-view/>
   </div>
 </template>
-
+<style lang="less">
+    @import "./assets/less/app.less";
+</style>
 <script>
+  import Nav from '@/components/common/Nav';
   export default {
-    name: 'app'
+    name: 'app',
+    components:{
+      Nav
+    }
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-</style>
---
+++ src/components/page/Pos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="pos">
+    Hello Pos 234 !!!
+    <i class="icon iconfont icon-hanbao"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pos'
+}
+</script>
+
+
