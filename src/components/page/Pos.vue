bfaf5fe56e06f99692af4bb4fc8e38b2f8439dd9
SawWu <2804265880@qq.com>
2018-01-07 18:23:51 +0800

    构建商品列表

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -4,20 +4,24 @@
       <el-col :span='7' class="pos-order">
         <el-tabs>
           <el-tab-pane label="点餐">
-            <el-table :data="tableData" border show-summary style="width: 100%" >
+            <el-table :data="tableData" border style="width: 100%" >
               <el-table-column prop="goodsName" label="商品"></el-table-column>
               <el-table-column prop="count" label="数量"></el-table-column>
               <el-table-column prop="price" label="金额"></el-table-column>
               <el-table-column prop="operation" label="操作" width="100" fixed="right">
                 <template slot-scope="scope">
-                  <el-button type="text" size="small">删除</el-button>
-                  <el-button type="text" size="small">增加</el-button>
+                  <el-button type="text" size="small" @click="delGoods(scope.row)">删除</el-button>
+                  <el-button type="text" size="small" @click="addOrderList(scope.row)">增加</el-button>
                 </template>
               </el-table-column>
             </el-table>
+            <div class="total">
+              <span>数量：{{totalCount}}</span>
+              <span>金额：{{totalMoney}}元</span>
+            </div>
             <div class="operation-btn">
               <el-button type="warning">挂单</el-button>
-              <el-button type="danger">删除</el-button>
+              <el-button type="danger" @click="delAllGoods()">删除</el-button>
               <el-button type="success">结账</el-button>
             </div>
           </el-tab-pane>
@@ -34,7 +38,7 @@
             <div class="title">常用商品</div>
             <div class="often-goods-list">
               <ul>
-                <li v-for="data in oftenGoods">
+                <li v-for="data in oftenGoods" @click="addOrderList(data)">
                   <span v-text="data.goodsName"></span>
                   <span class="o-price">￥{{data.price}}元</span>
                 </li>
@@ -45,7 +49,7 @@
              <el-tabs>
                <el-tab-pane label="汉堡">
                  <ul class='cookList'>
-                   <li v-for="data in type1Goods">
+                   <li v-for="data in type1Goods" @click="addOrderList(data)">
                      <span class="foodImg">
                          <img :src="data.goodsImg" width="100%" />
                      </span>
@@ -56,7 +60,7 @@
                </el-tab-pane>
                <el-tab-pane label="小食">
                  <ul class='cookList'>
-                   <li v-for="data in type2Goods">
+                   <li v-for="data in type2Goods" @click="addOrderList(data)">
                      <span class="foodImg">
                        <img :src="data.goodsImg" width="100%" />
                      </span>
@@ -67,7 +71,7 @@
                </el-tab-pane>
                <el-tab-pane label="饮料">
                  <ul class='cookList'>
-                   <li v-for="data in type3Goods">
+                   <li v-for="data in type3Goods" @click="addOrderList(data)">
                      <span class="foodImg">
                        <img :src="data.goodsImg" width="100%" />
                      </span>
@@ -78,7 +82,7 @@
                </el-tab-pane>
                <el-tab-pane label="套餐">
                  <ul class='cookList'>
-                    <li v-for="data in type4Goods">
+                    <li v-for="data in type4Goods" @click="addOrderList(data)">
                       <span class="foodImg">
                         <img :src="data.goodsImg" width="100%" />
                       </span>
@@ -102,28 +106,14 @@
     name: 'pos',
     data () {
       return {
-        tableData: [{
-          goodsName: '可口可乐',
-          price: 8,
-          count:1
-        },{
-          goodsName: '香辣鸡腿堡',
-          price: 15,
-          count:1
-        },{
-          goodsName: '爱心薯条',
-          price: 8,
-          count:1
-        },{
-          goodsName: '甜筒',
-          rice: 8,
-          count:1
-        }],
+        tableData:[],
         oftenGoods:[],
         type1Goods:[],
         type2Goods:[],
         type3Goods:[],
         type4Goods:[],
+        totalCount:0,
+        totalMoney:0
       }
     },
     created(){
@@ -141,7 +131,7 @@
         this.type2Goods=response.data[1];
         this.type3Goods=response.data[2];
         this.type4Goods=response.data[3];
-        console.log(response);
+
       })
       .catch((error)=> {
         console.log(error);
@@ -150,6 +140,55 @@
     mounted(){
       let orderHeight=document.body.clientHeight || document.documentElement.clientHeight;
       document.getElementsByClassName("pos-order")[0].style.height=orderHeight+'px';
+    },
+    methods:{
+      addOrderList(goods){
+        this.totalCount=0;
+        this.totalMoney=0;
+        let isHave=false;
+          //判断是否这个商品已经存在于订单列表
+        for (let i=0; i<this.tableData.length;i++){
+           if(this.tableData[i].goodsId==goods.goodsId){
+              isHave=true; //存在
+           }
+        }
+        //根据isHave的值判断订单列表中是否已经有此商品
+        if(isHave){
+          //存在就进行数量添加
+          let arr = this.tableData.filter(o =>o.goodsId == goods.goodsId);
+          arr[0].count++;
+        }else{
+          //不存在就推入数组
+          let newGoods={goodsId:goods.goodsId,goodsName:goods.goodsName,price:goods.price,count:1};
+          this.tableData.push(newGoods);
+        }
+
+        this.getAllMoney();
+      },
+
+      delGoods(goods){
+        console.log(goods);
+        this.tableData=this.tableData.filter(o => o.goodsId !=goods.goodsId);
+        this.getAllMoney();
+      },
+
+      delAllGoods() {
+        this.tableData = [];
+        this.totalCount = 0;
+        this.totalMoney = 0;
+      },
+
+      //汇总数量和金额
+      getAllMoney(){
+        this.totalCount = 0;
+        this.totalMoney = 0;
+        if(this.tableData) {
+          this.tableData.forEach((element) => {
+          this.totalCount += element.count;
+          this.totalMoney = this.totalMoney + (element.price * element.count);
+          });
+        }
+      }
     }
   }
 </script>
